0a3dff955ad94915346f70b69445c0e10a03172a
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-06 08:42:14 +0100

    create tag module (with controller and service), create absolute path, config package nodemon and tsconfig json files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Server works on port: ${PORT}`));
 }
 bootstrap();
