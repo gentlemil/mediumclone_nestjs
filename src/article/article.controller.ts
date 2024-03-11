932df20e976b9cbe56fce85901fa4f0f7ddf2232
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-11 18:18:59 +0100

    create article method part1/2

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { CreateArticleDto } from './dto/createArticle.dto';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { User } from '@app/user/decorators/user.decorator';
+import { UserEntity } from '@app/user/user.entity';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
+
   @Post()
-  async create() {
-    return await this.articleService.createArticle();
+  @UseGuards(AuthGuard)
+  async create(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<any> {
+    return await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,31 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { ArticleEntity } from './article.entity';
+import { CreateArticleDto } from './dto/createArticle.dto';
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from '@app/user/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ArticleService {
-  async createArticle() {
-    return 'create article from service';
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    console.log(currentUser, createArticleDto);
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.slug = 'fooo';
+    article.author = currentUser;
+
+    return await this.articleRepository.save(article);
   }
 }
