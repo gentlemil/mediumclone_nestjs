4b70bb87e9dd0f0fd742b10c65468f2a6af70ece
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-13 11:57:19 +0100

    small fix backend validation, update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import { AppModule } from './app.module';
 const PORT = 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // app.enableCors({
+  //   origin: '*',
+  // });
   await app.listen(PORT, () => console.log(`Server works on port: ${PORT}`));
 }
 bootstrap();
